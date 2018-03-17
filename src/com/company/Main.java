d31c39bed371479469579735d9b978537058763b
Alex Hand <alex.j.hand@gmail.com>
2018-03-16 19:57:27 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Your job is to create a simple banking application.
+        // There should be a bank class
+        // It should have an arraylist of Branches
+        // Each Branch should have an arraylist of customers
+        // The Customer class should have an arraylist of Doubles (transactions)
+        // Customer:
+        // Name, and the arraylist of doubles
+        // Branch:
+        // Need to be able to add a new customer and initial transaction amount
+        // Also needs to add additional transactions for that customer/branch
+        // Bank:
+        // Add a new branch
+        // Add a customer to that branch with an initial transaction
+        // Add a transaction for an existing customer for that branch
+        // Show a list of customers for a particular branch and optionally a list
+        // of their transactions
+        // Demonstrate autoboxing and unboxing in your code
+        // HINT: transactions
+        // Add data validation
+        // e.g. check if it exists, or does not exist
+        // Think about where you are adding code to perform certain actions.
+    }
+}
